381f86b9f4210ddb97c5f0c0f030d28faeb5e232
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-09 17:15:20 +0700

    ENHANCEMENT: Tests for all of the grid field action test

--- tests/CommentsGridFieldActionTest.php
+++ tests/CommentsGridFieldActionTest.php
@@ -21,7 +21,6 @@ class CommentsGridFieldActionTest extends SapphireTest {
 
 	public function testAugmentColumns() {
         $action = new CommentsGridFieldAction();
-        $record = new Comment();
 
         // an entry called 'Actions' is added to the columns array
         $columns = array();
@@ -43,23 +42,91 @@ class CommentsGridFieldActionTest extends SapphireTest {
 	}
 
 	public function testGetColumnMetadata() {
-		$this->markTestSkipped('TODO');
+		$action = new CommentsGridFieldAction();
+        $result = $action->getColumnMetadata($this->gridField, 'Actions');
+        $this->assertEquals(array('title' => ''), $result);
+        $result = $action->getColumnMetadata($this->gridField, 'SomethingElse');
+        $this->assertNull($result);
 	}
 
 	public function testGetColumnsHandled() {
-		$this->markTestSkipped('TODO');
+		$action = new CommentsGridFieldAction();
+        $result = $action->getColumnsHandled($this->gridField);
+        $this->assertEquals(array('Actions'), $result);
 	}
 
 	public function testGetColumnContent() {
-		$this->markTestSkipped('TODO');
+		$action = new CommentsGridFieldAction();
+        $record = new Comment();
+        $record->Name = 'Name of commeter';
+        $record->Comment = 'This is a comment';
+        $record->write();
+        $recordID = $record->ID;
+        $html = $action->getColumnContent($this->gridField, $record, 'Comment');
+        $this->assertContains('data-url="Controller/mockform/field/testfield',
+                                $html);
+        $spamAction = 'value="Spam" class="action" id="action_CustomAction' .
+                    $recordID . 'Spam"';
+        $this->assertContains($spamAction, $html);
+
+        $approveAction = 'value="Approve" class="action" id="action_CustomAction' .
+                    $recordID . 'Approve"';
+        $this->assertContains($approveAction, $html);
+
+        // If marked as spam, only the approve button should be available
+        $record->markSpam();
+        $record->write();
+        $html = $action->getColumnContent($this->gridField, $record, 'Comment');
+        $this->assertContains($approveAction, $html);
+        $this->assertNotContains($spamAction, $html);
+
+        // If marked as spam, only the approve button should be available
+        $record->markApproved();
+        $record->write();
+        $html = $action->getColumnContent($this->gridField, $record, 'Comment');
+        $this->assertNotContains($approveAction, $html);
+        $this->assertContains($spamAction, $html);
 	}
 
 	public function testGetActions() {
-		$this->markTestSkipped('TODO');
+		$action = new CommentsGridFieldAction();
+        $result = $action->getActions($this->gridField);
+        $this->assertEquals(array('spam', 'approve'), $result);
 	}
 
+
 	public function testHandleAction() {
-		$this->markTestSkipped('TODO');
+		$action = new CommentsGridFieldAction();
+        $record = new Comment();
+        $record->Name = 'Name of commeter';
+        $record->Comment = 'This is a comment';
+        $record->write();
+        $recordID = $record->ID;
+        $arguments = array('RecordID' => $recordID);
+        $data = array();
+        $result = $action->handleAction($this->gridField, 'spam', $arguments, $data );
+        $this->assertEquals(200, Controller::curr()->getResponse()->getStatusCode());
+        $this->assertEquals(
+            'Comment marked as spam.',
+            Controller::curr()->getResponse()->getStatusDescription()
+        );
+        $record = DataObject::get_by_id('Comment', $recordID);
+        $this->assertEquals(1, $record->Moderated);
+        $this->assertEquals(1, $record->IsSpam);
+
+//getStatusDescription
+        $result = $action->handleAction($this->gridField, 'approve', $arguments, $data );
+        $this->assertEquals(200, Controller::curr()->getResponse()->getStatusCode());
+        $this->assertEquals(
+            'Comment approved.',
+            Controller::curr()->getResponse()->getStatusDescription()
+        );
+
+        $record = DataObject::get_by_id('Comment', $recordID);
+        $this->assertEquals(1, $record->Moderated);
+        $this->assertEquals(0, $record->IsSpam);
+
+        error_log(Controller::curr()->getResponse()->getStatusCode());
 	}
 
 }
