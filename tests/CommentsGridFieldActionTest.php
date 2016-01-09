303f736c968762c52f5290edb842f1e2dba13de0
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-09 15:12:14 +0700

    WIP: 2 tests done, more coming

--- tests/CommentsGridFieldActionTest.php
+++ tests/CommentsGridFieldActionTest.php
@@ -1,12 +1,45 @@
 <?php
 
 class CommentsGridFieldActionTest extends SapphireTest {
+
+    /** @var ArrayList */
+    protected $list;
+
+    /** @var GridField */
+    protected $gridField;
+
+    /** @var Form */
+    protected $form;
+
+     public function setUp() {
+        parent::setUp();
+        $this->list = new DataList('GridFieldAction_Delete_Team');
+        $config = CommentsGridFieldConfig::create()->addComponent(new GridFieldDeleteAction());
+        $this->gridField = new CommentsGridField('testfield', 'testfield', $this->list, $config);
+        $this->form = new Form(new Controller(), 'mockform', new FieldList(array($this->gridField)), new FieldList());
+    }
+
 	public function testAugmentColumns() {
-		$this->markTestSkipped('TODO');
+        $action = new CommentsGridFieldAction();
+        $record = new Comment();
+
+        // an entry called 'Actions' is added to the columns array
+        $columns = array();
+        $action->augmentColumns($this->gridField, $columns);
+        $expected = array('Actions');
+        $this->assertEquals($expected, $columns);
+
+        $columns = array('Actions');
+        $action->augmentColumns($this->gridField, $columns);
+        $expected = array('Actions');
+        $this->assertEquals($expected, $columns);
 	}
 
 	public function testGetColumnAttributes() {
-		$this->markTestSkipped('TODO');
+		$action = new CommentsGridFieldAction();
+        $record = new Comment();
+        $attrs = $action->getColumnAttributes($this->gridField, $record, 'Comment');
+        $this->assertEquals(array('class' => 'col-buttons'), $attrs);
 	}
 
 	public function testGetColumnMetadata() {
